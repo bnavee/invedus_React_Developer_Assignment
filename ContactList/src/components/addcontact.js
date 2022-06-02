f3cc5b2365fdf33d6614af58b272daeac0c2576f
bnavee <naveenbojja@hotmail.com>
2022-06-02 16:34:38 +0530

    Invedus contacts list

---
+++ ContactList/src/components/addcontact.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+const AddPost = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [phoneType, setPhoneType] = useState("");
+  const [whatsApp, setWhatsApp] = useState(false);
+  
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkContactEmailExists = contacts.filter((contact) =>
+      contact.email === email ? contact : null
+    );
+    const checkContactPhoneExists = contacts.filter((contact) =>
+      contact.phone === phone ? contact : null
+    );
+
+    if (!email || !name || !phone) {
+      return toast.warning("Please fill in all fields!!");
+    }
+    if (checkContactEmailExists.length > 0) {
+      return toast.error("This email already exists!!");
+    }
+    if (checkContactPhoneExists.length > 0) {
+      return toast.error("This phone number already exists!!");
+    }
+
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+      phoneType,
+      whatsApp,
+    };
+
+    addContact(data);
+    toast.success("Contact added successfully!!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-dark py-3 display-2">Add Contact</h1>
+      <div className="row">
+        <div className="col-md-6 p-5 mx-auto shadow">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Full name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+            Type: 
+              <select onChange={(e) => setPhoneType(e.target.value)} name="phoneType">
+                
+                <option value="personal" >Personal</option>
+                <option value="office" >Office</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>
+                Is WhatsApp:
+                <input
+                  name="isWhatsApp"
+                  type="checkbox"
+                  checked={whatsApp}
+                  onChange={(e) => setWhatsApp(!whatsApp)}
+              />
+              </label>
+            </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Add Contact"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state,
+});
+const mapDispatchToProps = (dispatch) => ({
+  addContact: (data) => {
+    dispatch({ type: "ADD_CONTACT", payload: data });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPost);
